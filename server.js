ddfa4d94a020dc209bfd447a058746779acb00a4
Tansuani Waring <tansuanigomezw@gmail.com>
2023-12-13 20:33:19 -0300

    inserting get, post and delete

--- server.js
+++ server.js
@@ -6,15 +6,30 @@ app.use(express.json())
 app.use(cors())
 
 app.get("/home", (req, res) => {res.send("hello World Express Js")})
-app.get("/productos", (req, res) => {
+app.get("/cansiones", (req, res) => {
     try {
-        const productos = JSON.parse(fs.readFileSync("productos.json"));
-        res.json(productos);
+        const cansiones = JSON.parse(fs.readFileSync("cansiones.json"));
+        res.json(cansiones);
     } catch (error) {
         res.status(500).json({error: "Error al procesar la solicitud"});
         console.error("Error al procesar la solicitud", error);
     }
 });
 
+app.post("/cansiones", (req, res) => {
+    const cansion = req.body;
+    const cansiones = JSON.parse(fs.readFileSync("cansiones.json", "utf8"));
+    cansiones.push(cansion);
+    fs.writeFileSync("cansiones.json", JSON.stringify(cansiones));
+    res.send("cansion agregada")
+})
+
+app.delete("/cansiones/:id", (req, res) => {
+    const { id } = req.params;
+    const cansiones = JSON.parse(fs.readFileSync("cansiones.json", "utf8"));
+    const index = cansiones.findIndex(cansion => cansion.id === id);
+    cansiones.splice(index, 1);
+    fs.writeFileSync("cansiones.json", JSON.stringify(cansiones));
+})
 app.listen(PORT, console.log("¡Servidor encendido"))
 
