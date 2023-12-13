9b842c81249ff707122386a19ea36ec106f04a89
Tansuani Waring <tansuanigomezw@gmail.com>
2023-12-12 22:53:23 -0300

    validating middlewares and cors

--- server.js
+++ server.js
@@ -2,7 +2,19 @@ import express from "express"
 const app = express()
 const PORT = process.env.PORT || 3000
 
+app.use(express.json())
+app.use(cors())
+
 app.get("/home", (req, res) => {res.send("hello World Express Js")})
+app.get("/productos", (req, res) => {
+    try {
+        const productos = JSON.parse(fs.readFileSync("productos.json"));
+        res.json(productos);
+    } catch (error) {
+        res.status(500).json({error: "Error al procesar la solicitud"});
+        console.error("Error al procesar la solicitud", error);
+    }
+});
 
 app.listen(PORT, console.log("¡Servidor encendido"))
 
